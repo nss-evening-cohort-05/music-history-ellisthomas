1165069b2e55db483804adf93d7e44e89d9432c4
ellisthomas <ellisthomas@gmail.com>
2017-02-16 21:52:03 -0600

    added two songs to array loop and replaced special characters

--- songs.js
+++ songs.js
@@ -1,8 +1,20 @@
 var songs = [];
-
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
+var newSongs = songs.push("Thriller by Michael Jackson on the album Thiller");
+var newSongs = songs.unshift("Let's Groove by Earth Wind & Fire on the album Rise");
+
+for (var i = 0; i < songs.length; i++) {
+	songs[i] = songs[i].replace(" >", " -");
+	songs[i] = songs[i].replace("*", "");
+	songs[i] = songs[i].replace("@", "");
+	songs[i] = songs[i].replace("(", "");
+	songs[i] = songs[i].replace("!", "");
+}
+
+var songs = document.getElementById("tracks");
+console.log(songs);
