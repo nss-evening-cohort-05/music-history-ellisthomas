495e9f16abaae47d532c4634347ec1bfa6040354
ellisthomas <ellisthomas@gmail.com>
2017-02-17 12:29:23 -0600

    got one song to print to the DOM

--- songs.js
+++ songs.js
@@ -14,7 +14,7 @@ for (var i = 0; i < songs.length; i++) {
 	songs[i] = songs[i].replace("@", "");
 	songs[i] = songs[i].replace("(", "");
 	songs[i] = songs[i].replace("!", "");
+
+	document.getElementById("tracks").innerHTML = songs[i];
 }
 
-var songs = document.getElementById("tracks");
-console.log(songs);
